b09efa8dd417906d4310f3f19d5c796f44b24532
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-06 01:52:27 +0200

    Use -Wl,--undefined

--- cmake/win/WinFunctions.cmake
+++ cmake/win/WinFunctions.cmake
@@ -2,8 +2,8 @@ function(librw_platform_target TARGET)
     cmake_parse_arguments(LPT "PROVIDES_WINMAIN" "" "" ${ARGN})
     get_target_property(TARGET_TYPE "${TARGET}" TYPE)
     if(MINGW AND LPT_PROVIDES_WINMAIN AND TARGET_TYPE STREQUAL "STATIC_LIBRARY")
-        # When consumers link to this target, make the linker require WinMain to be defined.
-        # This causes WinMain to not be dropped.
-        target_link_options(${TARGET} INTERFACE -Wl,--require-defined=WinMain)
+        # Start with WinMain marked as undefined.
+        # This makes the linker keep a WinMain symbol from a static library.
+        target_link_options(${TARGET} INTERFACE -Wl,--undefined,WinMain)
     endif()
 endfunction()
