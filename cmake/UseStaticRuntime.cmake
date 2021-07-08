8026a2028aadf8a311a09ca0d335a5e52230904e
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-08 16:12:17 +0200

    static runtime: add only -static

--- cmake/UseStaticRuntime.cmake
+++ cmake/UseStaticRuntime.cmake
@@ -5,9 +5,5 @@ if(CMAKE_C_COMPILER_ID MATCHES "^MSVC$")
 endif()
 
 if(CMAKE_C_COMPILER_ID MATCHES "^GNU$")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++ -static")
-endif()
-
-if(WIN32)
-    list(REMOVE_ITEM CMAKE_FIND_LIBRARY_SUFFIXES .dll.a .dll.lib)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
 endif()
