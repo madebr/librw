313b1dcbaa8faeb0e34e5aa46dbc9a690a2d261d
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-09 19:24:03 +0200

    ps2 cmake fixes

--- cmake/ps2/PS2Functions.cmake
+++ cmake/ps2/PS2Functions.cmake
@@ -2,6 +2,11 @@ if(NOT COMMAND add_erl_executable)
     message(FATAL_ERROR "The `add_erl_executable` cmake command is not available. Please use an appropriate Playstation 2 toolchain.")
 endif()
 
+#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -flto")
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto")
+#set(CMAKE_DSM_FLAGS "${CMAKE_DSM_FLAGS} -flto")
+#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -flto")
+
 function(librw_platform_target TARGET)
     cmake_parse_arguments(LPT "INSTALL" "" "" ${ARGN})
 
